75a03a7f8f56fb25291fb5d55cce98f653b20f78
Łukasz Piątkowski <piontec@gmail.com>
2020-06-18 13:21:57 +0200

    support for the new build layout (since 5.7.2)

--- uuku.py
+++ uuku.py
@@ -27,9 +27,9 @@ class SingleVerHTMLParser(HTMLParser):
         self.version = version
         self.files = []
 
-        self.arch_start_regexp = re.compile(".*Build for (.+) succeeded.*")
+        self.arch_start_regexp = re.compile(".*(Build for|Test) (\w+)(\/build)? succeeded.*")
         self.file_name_regexp = re.compile(
-            "^linux.*{}.*((_all\.deb)|(generic.*_{}\.deb))".format(version, arch))
+            "({}\/)?linux.*{}.*((_all\.deb)|(generic.*_{}\.deb))".format(arch,version, arch))
         self.arch_found = False
         self.correct_arch_found = False
 
@@ -37,7 +37,7 @@ class SingleVerHTMLParser(HTMLParser):
         arch_matched = self.arch_start_regexp.match(data)
         if arch_matched is not None:
             self.arch_found = True
-            if arch_matched.group(1) == self.arch:
+            if arch_matched.group(2) == self.arch:
                 self.correct_arch_found = True
             else:
                 self.correct_arch_found = False
@@ -71,21 +71,25 @@ last_ver_parser = SingleVerHTMLParser(arch, flavor, last_ver)
 last_ver_parser.feed(str(last_ver_page.content))
 
 dirpath = tempfile.mkdtemp()
+downloaded = []
 for file in last_ver_parser.files:
-    dst_file = filename = "{}/{}".format(dirpath, file)
+    filename = file[len(arch) + 1:] if file.startswith(arch) else file
+    dst_file = "{}/{}".format(dirpath, filename)
     print("Fetching: ", dst_file)
     urlretrieve("https://kernel.ubuntu.com/~kernel-ppa/mainline/v{}/{}".format(
         last_ver, file), dst_file)
-installation_order = ["linux-headers", "linux-modules", "linux-image"]
-for file_prefix in installation_order:
-    files = [f for f in last_ver_parser.files if f.startswith(file_prefix)]
+    downloaded.append(filename)
+installation_order = ["linux-headers-.*_all", f"linux-headers-.*-{flavor}", f"linux-modules.*-{flavor}",
+                      f"linux-image.*-{flavor}"]
+for file_prefix_regexp in [re.compile(x) for x in installation_order]:
+    files = [f for f in downloaded if file_prefix_regexp.match(f)]
     for file in files:
-        dst_file = filename = "{}/{}".format(dirpath, file)
+        dst_file = "{}/{}".format(dirpath, file)
         print("Installing: ", dst_file)
         run_res = subprocess.run(["sudo", "dpkg", "-i", dst_file])
 shutil.rmtree(dirpath)
 
-for file in last_ver_parser.files:
+for file in downloaded:
     if file.startswith("linux-image"):
         image_name = "/boot/vmlinuz-" + \
             "-".join(file.split("_")[0].split("-")[3:])
