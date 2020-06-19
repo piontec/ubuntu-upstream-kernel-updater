851acc96b5e758858e7205f1c7cc5340dc1fc326
Łukasz Piątkowski <piontec@gmail.com>
2020-06-19 11:53:51 +0200

    support for command line options, it's possible to choose version prefix now

--- uuku.py
+++ uuku.py
@@ -1,18 +1,31 @@
-import requests
+import argparse
 import re
-from html.parser import HTMLParser
-from urllib.request import urlretrieve
-import tempfile
 import shutil
 import subprocess
 import sys
+import tempfile
+from html.parser import HTMLParser
+from urllib.request import urlretrieve
+
+import requests
 
 
 class VersionIndexHTMLParser(HTMLParser):
-    def __init__(self, *, convert_charrefs=True):
+    def __init__(self, prefix, *, convert_charrefs=True):
         super().__init__(convert_charrefs=convert_charrefs)
         self.versions = []
-        self.ver_regexp = re.compile('^v\d+\.\d+(\.\d+)?/$')
+        if prefix is None:
+            self.ver_regexp = re.compile('^v\d+\.\d+(\.\d+)?/$')
+        else:
+            matched = re.match('^(\d+)(\.\d+)?$', prefix)
+            if not matched:
+                print(f"Version prefix must match pattern 'X.Y' or just 'X', but '{prefix}' was found.")
+                sys.exit(1)
+            groups = matched.groups()
+            if groups[1] is None:
+                self.ver_regexp = re.compile(f'^v{groups[0]}\.\d+(\.\d+)?/$')
+            else:
+                self.ver_regexp = re.compile(f'^v{groups[0]}\.{groups[1][1:]}(\.\d+)?/$')
 
     def handle_data(self, data):
         if self.ver_regexp.match(data):
@@ -28,8 +41,7 @@ class SingleVerHTMLParser(HTMLParser):
         self.files = []
 
         self.arch_start_regexp = re.compile(".*(Build for|Test) (\w+)(\/build)? succeeded.*")
-        self.file_name_regexp = re.compile(
-            "({}\/)?linux.*{}.*((_all\.deb)|(generic.*_{}\.deb))".format(arch,version, arch))
+        self.file_name_regexp = re.compile(f"({arch}\/)?linux.*{version}.*((_all\.deb)|(generic.*_{arch}\.deb))")
         self.arch_found = False
         self.correct_arch_found = False
 
@@ -46,10 +58,17 @@ class SingleVerHTMLParser(HTMLParser):
                 self.files.append(data)
 
 
-arch = "amd64"
-flavor = 'generic'
+parser = argparse.ArgumentParser(description="Install upstream ubuntu kernel")
+parser.add_argument("-p", "--prefix", type=str, help="install only from release versions matching the prefix")
+parser.add_argument("-a", "--arch", type=str, help="use kernel for specific architecture", default="amd64")
+parser.add_argument("-f", "--flavor", type=str, help="use kernel for of specific flavor (default: generic)",
+                    default="generic", choices=["generic", "lowlatency"])
+parser.add_argument("-s", "--sign", type=bool, help="should the kernel be signed after installing (default: True)",
+                    default=True, choices=[True, False])
+args = parser.parse_args()
+
 index = requests.get('https://kernel.ubuntu.com/~kernel-ppa/mainline/')
-index_parser = VersionIndexHTMLParser()
+index_parser = VersionIndexHTMLParser(args.prefix)
 index_parser.feed(str(index.content, 'utf-8'))
 sorted_vers = sorted(
     index_parser.versions, key=lambda s: list(map(int, s.split('.'))))
@@ -65,34 +84,33 @@ if last_ver == local_ver:
     "You're already on the latest version, nothing to do"
     sys.exit(0)
 
-last_ver_page = requests.get(
-    'https://kernel.ubuntu.com/~kernel-ppa/mainline/v{}/'.format(last_ver))
-last_ver_parser = SingleVerHTMLParser(arch, flavor, last_ver)
+last_ver_page = requests.get( f"https://kernel.ubuntu.com/~kernel-ppa/mainline/v{last_ver}/")
+last_ver_parser = SingleVerHTMLParser(args.arch, args.flavor, last_ver)
 last_ver_parser.feed(str(last_ver_page.content))
 
 dirpath = tempfile.mkdtemp()
 downloaded = []
 for file in last_ver_parser.files:
-    filename = file[len(arch) + 1:] if file.startswith(arch) else file
-    dst_file = "{}/{}".format(dirpath, filename)
+    filename = file[len(args.arch) + 1:] if file.startswith(args.arch) else file
+    dst_file = f"{dirpath}/{filename}"
     print("Fetching: ", dst_file)
-    urlretrieve("https://kernel.ubuntu.com/~kernel-ppa/mainline/v{}/{}".format(
-        last_ver, file), dst_file)
+    urlretrieve(f"https://kernel.ubuntu.com/~kernel-ppa/mainline/v{last_ver}/{file}", dst_file)
     downloaded.append(filename)
-installation_order = ["linux-headers-.*_all", f"linux-headers-.*-{flavor}", f"linux-modules.*-{flavor}",
-                      f"linux-image.*-{flavor}"]
+installation_order = ["linux-headers-.*_all", f"linux-headers-.*-{args.flavor}", f"linux-modules.*-{args.flavor}",
+                      f"linux-image.*-{args.flavor}"]
 for file_prefix_regexp in [re.compile(x) for x in installation_order]:
     files = [f for f in downloaded if file_prefix_regexp.match(f)]
     for file in files:
-        dst_file = "{}/{}".format(dirpath, file)
+        dst_file = f"{dirpath}/{file}"
         print("Installing: ", dst_file)
         run_res = subprocess.run(["sudo", "dpkg", "-i", dst_file])
 shutil.rmtree(dirpath)
 
-for file in downloaded:
-    if file.startswith("linux-image"):
-        image_name = "/boot/vmlinuz-" + \
-            "-".join(file.split("_")[0].split("-")[3:])
-        print("Singing kernel image: " + image_name)
-        subprocess.run(["sudo", "sbsign", "--key", "/root/mok/MOK.priv", "--cert",
-                        "/root/mok/MOK.pem", image_name, "--output", image_name])
+if args.sign:
+    for file in downloaded:
+        if file.startswith("linux-image"):
+            image_name = "/boot/vmlinuz-" + \
+                         "-".join(file.split("_")[0].split("-")[3:])
+            print("Singing kernel image: " + image_name)
+            subprocess.run(["sudo", "sbsign", "--key", "/root/mok/MOK.priv", "--cert",
+                            "/root/mok/MOK.pem", image_name, "--output", image_name])
